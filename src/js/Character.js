2cdf6eb19264cd6ca53728d05858b027539d9db8
Regina Salikhova <yanadera1873@gmail.com>
2025-08-06 14:54:54 +0400

    added character classes and modified the Character class

--- src/js/Character.js
+++ src/js/Character.js
@@ -14,11 +14,24 @@
  */
 export default class Character {
   constructor(level, type = 'generic') {
+    // TODO: выбросите исключение, если кто-то использует "new Character()"
+    if(new.target === Character) {
+      throw new Error("You cannot create instances of the Character base class directly");
+    }
+    // Уровень персонажа только от 1 до 4
+    if (typeof level !== 'number' || level < 1 || level > 4) {
+      throw new Error("Level must be a number between 1 and 4");
+    }
+    // строка с одним из допустимых значений
+    const validTypes = ["swordsman", "bowman", "magician", "daemon", "undead", "vampire"];
+    if(!validTypes.includes(type)) {
+      throw new Error("Invalid character type");    
+    }
+
     this.level = level;
     this.attack = 0;
     this.defence = 0;
     this.health = 50;
     this.type = type;
-    // TODO: выбросите исключение, если кто-то использует "new Character()"
   }
 }
