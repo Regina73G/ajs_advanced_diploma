354b822c2544d9e4da49446fdf39b8cc3d385933
Regina Salikhova <yanadera1873@gmail.com>
2025-08-07 15:09:22 +0400

    added auto test for GamePlay.showCharacterInfo()

--- src/js/GamePlay.js
+++ src/js/GamePlay.js
@@ -230,10 +230,10 @@ export default class GamePlay {
   }
 
   showCharacterInfo(character) {
-    const level = "\u{1F396}" + character.level;
-    const attack = "\u2694" + character.attack;
-    const defence = "\u{1F6E1}" + character.defence;
-    const health = "\u2764" + character.health;
+    const level = "\u{1F396}" + " " + character.level;
+    const attack = "\u2694" + " " + character.attack;
+    const defence = "\u{1F6E1}" + " " + character.defence;
+    const health = "\u2764" + " " + character.health;
     return `${level} ${attack} ${defence} ${health}`;
   }
 }
---
+++ src/js/__tests__/GamePlay.test.js
@@ -0,0 +1,10 @@
+import GamePlay from "../GamePlay";
+import Bowman from "../characters/Bowman";
+
+test("", () => {
+  const gameplay = new GamePlay();
+  const character = new Bowman(1);
+  const result = gameplay.showCharacterInfo(character);
+  const expectedResult = `${"\u{1F396}"} 1 ${"\u2694"} 25 ${"\u{1F6E1}"} 25 ${"\u2764"} 50`
+  expect(result).toEqual(expectedResult);
+});
